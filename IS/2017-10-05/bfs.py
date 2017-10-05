1148b736fe9e5b99a116dd4e94f350485722ba39
Valentin Kirilov <val.kirilov@gmail.com>
2017-10-05 19:32:44 +0300

    IS excercise from 2017-10-05
    - simple DFS and BFS algorythms

---
+++ IS/2017-10-05/bfs.py
@@ -0,0 +1,58 @@
+
+from node import Node
+from collections import deque
+
+# Build the nodes
+n1 = Node(1)
+n2 = Node(2)
+n3 = Node(3)
+n4 = Node(4)
+n5 = Node(5)
+n6 = Node(6)
+n7 = Node(7)
+n8 = Node(8)
+n9 = Node(9)
+n10 = Node(10)
+n11 = Node(11)
+n12 = Node(12)
+
+# Make the connections between the nodes
+n1.add_child(n2)
+n1.add_child(n3)
+n1.add_child(n4)
+
+n2.add_child(n5)
+n2.add_child(n6)
+
+n4.add_child(n7)
+n4.add_child(n8)
+
+n5.add_child(n9)
+n5.add_child(n10)
+
+n7.add_child(n11)
+n7.add_child(n12)
+
+# Basic dump of the top level of the tree
+#for c in n1.children:
+#    print c.data
+
+print('BFS')
+
+# BFS Algoruthm
+stack = deque([n1])
+visited = []
+
+while True:
+    if len(stack) == 0:
+        print('Done.')
+        break;
+
+    current = stack.popleft()
+    print(current.data)
+
+    for child in current.children:
+        if  child not in visited:
+            visited.append(child)
+            stack.append(child)
+
