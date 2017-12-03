6ea7afb0bd910cc9470b5fabbc6ae715cf214b29
Valentin Kirilov <val.kirilov@gmail.com>
2017-12-03 17:36:02 +0200

    IS homework 6
    - the solution now can work with the iris data set

--- IS/homework-6/hm6.py
+++ IS/homework-6/hm6.py
@@ -1,46 +1,52 @@
 
 import math
 import numpy as np
+import csv
+import random
 
-base_data = [
-    [1, 10, 0],
-    [2, 10, 0],
-    [3, 11, 0],
-    [2, 9, 0],
-    [3, 9, 0],
+base_data = []
+test_data = []
+trainess_data = []
 
-    [1, 2, 1],
-    [2, 2, 1],
-    [3, 3, 1],
-    [3, 2, 1],
 
-    [5, 5, 2],
-    [5, 6, 2],
-    [5, 7, 2],
-    [6, 6, 2],
-    [7, 7, 2],
-]
+def read_input_csv():
+    data = []
+    with open('iris.data.csv', 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            if row != []:
+                data.append(row)
+    return data
 
-input_data = [
-    [2, 11],
-    [2.5, 2],
-    [5.5, 6],
-]
+
+def split_input_data(input_data):
+    test_data = []
+    trainess_data = []
+
+    test_data = random.sample(input_data, 20)
+    trainess_data = [i for i in input_data if i not in test_data]
+
+    return test_data, trainess_data
+
+
+def print_data_set(data_set):
+    for i in range(len(data_set)):
+        print(data_set[i])
 
 
 def find_distance_between_dots(dot1, dot2):
     distance = 0
-    for i in range(len(dot2)):
-        distance += (dot2[i] - dot1[i]) * (dot2[i] - dot1[i])
+    for i in range(len(dot1)-1):
+        distance += (float(dot2[i]) - float(dot1[i])) * (float(dot2[i]) - float(dot1[i]))
     return math.sqrt(distance)
 
 
-def caluclate_distances(base_data, point):
+def caluclate_distances(trainees_data, point):
     distances = []
 
     # First calculate the distance between the new point and the base points
-    for i in range(len(base_data)):
-        distance = find_distance_between_dots(base_data[i], point)
+    for i in range(len(trainees_data)):
+        distance = find_distance_between_dots(trainees_data[i], point)
         distances.append(distance)
 
     # Next, normalize the calucalted distances
@@ -53,14 +59,14 @@ def caluclate_distances(base_data, point):
     return normalized_distances
 
 
-def find_k_nearest_neighbours(base_data, distances, point):
+def find_k_nearest_neighbours(trainees_data, distances, point):
     k = 3  # To DO
     distances_range = range(len(distances))
     sorted_distances = sorted(distances_range, key=lambda k: distances[k], reverse=True)
 
     found_point_types = {}
     for i in range(len(sorted_distances)):
-        point_type = base_data[sorted_distances[i]][2]
+        point_type = trainees_data[sorted_distances[i]][len(point)-1]
 
         if point_type not in found_point_types:
             found_point_types[point_type] = 0
@@ -70,10 +76,23 @@ def find_k_nearest_neighbours(base_data, distances, point):
             return point_type
 
 
-def find_input_data_types(base_data, input_data):
-    for i in range(len(input_data)):
-        distances = caluclate_distances(base_data, input_data[i])
-        #print(distances)
-        print(find_k_nearest_neighbours(base_data, distances, input_data[i]))
+def find_input_data_types(trainees_data, test_data):
+    success_rate = 0
+    for i in range(len(test_data)):
+        distances = caluclate_distances(trainees_data, test_data[i])
+        result = find_k_nearest_neighbours(trainees_data, distances, test_data[i])
+        expected = test_data[i][len(test_data[i])-1]
+        if result == expected:
+            success_rate += 1
+            print('   (E): ' + expected + '; (R): ' + result)
+        else:
+            print('Х: (E): ' + expected + '; (R): ' + result)
 
-find_input_data_types(base_data, input_data)
+    print()
+    print('Success rate: ' + str((success_rate/len(test_data))*100) + '%')
+
+
+base_data = read_input_csv()
+test_data, trainees_data = split_input_data(base_data)
+
+find_input_data_types(trainees_data, test_data)
