3aa8a0562c0824fb66965bfcb4dd5d4fa60b8309
Valentin Kirilov <val.kirilov@gmail.com>
2018-02-07 20:12:23 +0200

    Latest IS changes

--- IS/Project/digitsrcgn/digitsrcgn/algorithms/knn.py
+++ IS/Project/digitsrcgn/digitsrcgn/algorithms/knn.py
@@ -72,7 +72,7 @@ def caluclate_distances(trainees_data, point):
 
 
 def find_k_nearest_neighbours(trainees_data, distances, point):
-    k = 5  # To DO
+    k = 103  # To DO
 
     distances_range = range(len(distances))
     sorted_distances = sorted(distances_range, key=lambda k: distances[k], reverse=True)
@@ -116,19 +116,19 @@ def find_input_data_types(trainees_data, test_data):
 
             if result == expected:
                 results['successfully_classified'] += 1
-                print('   (E): ' + str(expected) + '; (R): ' + str(result))
-            else:
-                print('Х: (E): ' + str(expected) + '; (R): ' + str(result))
+                #print('   (E): ' + str(expected) + '; (R): ' + str(result))
+            #else:
+                #print('Х: (E): ' + str(expected) + '; (R): ' + str(result))
         else:
             results['classification'] = result
             print('   Classificated as ' + result)
 
-    with open(os.path.join(APP_ROOT, 'output/knn.csv'), 'wb') as csvfile:
-        output = csv.writer(csvfile, delimiter=',')
-        output.writerow(['ImageId', 'Label'])
+    # with open(os.path.join(APP_ROOT, 'output/knn.csv'), 'wb') as csvfile:
+    #     output = csv.writer(csvfile, delimiter=',')
+    #     output.writerow(['ImageId', 'Label'])
 
-        for i in range(len(test_data)):
-            output.writerow([i, int(output_results[i])])
+    #     for i in range(len(test_data)):
+    #         output.writerow([i, int(output_results[i])])
 
     results['accuracy'] = (float(results['successfully_classified']) / float(results['test_data_length'])) * 100
     return results
@@ -138,7 +138,7 @@ def clasiffy(input_test_data=None):
     if input_test_data is None:
         print('Start reading data')
         trainees_data = read_input_csv('datasets/train.lite.csv')
-        test_data = read_input_csv('datasets/test.csv', True)
+        test_data = read_input_csv('datasets/test.lite.csv', True)
         #print('Split data into two datasets')
         #test_data, trainees_data = split_input_data(base_data)
     else:
@@ -158,3 +158,69 @@ def clasiffy(input_test_data=None):
     results['execution_time'] = int(end_time - start_time)
 
     return results
+
+
+def separate_to_chunks(data_set, n):
+    data = list(data_set)
+    random.shuffle(data)
+    data_length = len(data)
+    chunk_size = int(data_length / n)
+
+    chunks = [data[0+chunk_size*i: chunk_size*(i+1)] for i in range(n)]
+    leftover = data_length - chunk_size*n
+    edge = chunk_size*n
+    for i in range(leftover):
+        chunks[i % n].append(data[edge+i])
+
+    return chunks
+
+
+def crossfold():
+    print('Start reading data')
+    base_data = read_input_csv('datasets/train.csv', False)
+    filtered_data = random.sample(base_data, 10000)
+    data_chunks = separate_to_chunks(filtered_data, 10)
+
+    print('Start classification')
+
+    start_time = time.time()
+
+    print('All' + str(len(filtered_data)))
+
+    overall_accuracy = 0
+    for i in range(len(data_chunks)):
+        trainess_data = data_chunks[i]
+        test_data = data_chunks[:i] + data_chunks[i+1:len(data_chunks)]
+
+        test_data = []
+        for j in range(len(data_chunks)):
+            if i != j:
+                test_data += data_chunks[j]
+
+        #print('Train ' + str(len(trainess_data)))
+        #print('Test ' + str(len(test_data)))
+
+        #results = find_input_data_types(trainess_data, test_data)
+        results = find_input_data_types(test_data, trainess_data)
+        accuracy = results['accuracy']
+        print(results)
+
+        overall_accuracy += accuracy
+        print('Chunk ' + str(i) + '; Accuracy: ' + str(accuracy) + '%')
+
+    overall_accuracy = overall_accuracy / len(data_chunks)
+    print('Overall accuracy: ' + str(overall_accuracy) + '%')
+
+    end_time = time.time()
+
+    print('Classification finished')
+    print(end_time - start_time)
+
+    results['execution_time'] = int(end_time - start_time)
+
+    return results
+
+
+if __name__ == "__main__":
+    print('KNN')
+    results = crossfold()
--- IS/Project/digitsrcgn/digitsrcgn/algorithms/nbc.py
+++ IS/Project/digitsrcgn/digitsrcgn/algorithms/nbc.py
@@ -1,9 +1,10 @@
-
+import os
 import math
 import numpy as np
 import csv
 import random
 from collections import Counter
+from operator import attrgetter
 
 test_data = []
 trainess_data = []
@@ -27,6 +28,11 @@ def read_input_csv(filename):
 def print_data_set(data_set):
     for i in range(len(data_set)):
         print(data_set[i])
+
+
+def print_attributes_probabilitirs(attributes_probabilities):
+    for i in range(len(attributes_probabilities)):
+        print(attributes_probabilities[i])
 
 
 def separate_to_chunks(data_set, n):
@@ -74,58 +80,175 @@ def init_classes_probabilities(data):
     }
 
 
+def mean(data):
+    return sum(data)/float(len(data))
+
+
+def stdev(data):
+    print(data)
+    avg = mean(data)
+    variance = sum([pow(x-avg,2) for x in data])/float(len(data)-1)
+    return math.sqrt(variance)
+
+
+def calculateProbability(x, mean, stdev):
+    exponent = math.exp(-(math.pow(x-mean,2)/(2*math.pow(stdev,2))))
+    return (1 / (math.sqrt(2*math.pi) * stdev)) * exponent
+
+
 def init_attribute_probabilities(data, attribute, item_class):
-    occuranciesCounter = {
-        '\'y\'': 0,
-        '\'n\'': 0,
-        '?': 0,
-    }
+    #print(attribute)
+    #print(item_class)
+    #print(len(data))
+
+    attribute_data = []
 
     for i in range(len(data)):
-        if data[i][len(data[i])-1] == item_class:
-            occuranciesCounter[data[i][attribute]] += 1
+        if data[i][0] == item_class:
+            attribute_data.append(float(data[i][attribute]))
 
-    return {
-        '\'y\'': occuranciesCounter['\'y\''] / len(data),
-        '\'n\'': occuranciesCounter['\'n\''] / len(data),
-    }
+    #print(attribute_data)
+
+    if len(attribute_data) > 0:
+        x = float(data[i][attribute])
+        m = mean(attribute_data)
+        sd = stdev(attribute_data)
+
+        if sd == 0:
+            probability = 1
+        else:
+            probability = calculateProbability(x, m, sd)
+
+        #print(m)
+        #print(sd)
+        #print(probability)
+    else:
+        probability = 1
+
+    return probability
 
 
 def init_attributes_probabilities(data):
     attributes_probabilities = []
 
     for i in range(len(data[0]) - 1):
-        attr_democart_prob = init_attribute_probabilities(data, i, '\'democrat\'')
-        attr_republican_prob = init_attribute_probabilities(data, i, '\'republican\'')
+        attr_0_prob = init_attribute_probabilities(data, i, '0')
+        attr_1_prob = init_attribute_probabilities(data, i, '1')
+        attr_2_prob = init_attribute_probabilities(data, i, '2')
+        attr_3_prob = init_attribute_probabilities(data, i, '3')
+        attr_4_prob = init_attribute_probabilities(data, i, '4')
+        attr_5_prob = init_attribute_probabilities(data, i, '5')
+        attr_6_prob = init_attribute_probabilities(data, i, '6')
+        attr_7_prob = init_attribute_probabilities(data, i, '7')
+        attr_8_prob = init_attribute_probabilities(data, i, '8')
+        attr_9_prob = init_attribute_probabilities(data, i, '9')
+
         attributes_probabilities.append({
-            '\'democrat\'': attr_democart_prob,
-            '\'republican\'': attr_republican_prob,
+            '0': attr_0_prob,
+            '1': attr_1_prob,
+            '2': attr_2_prob,
+            '3': attr_3_prob,
+            '4': attr_4_prob,
+            '5': attr_5_prob,
+            '6': attr_6_prob,
+            '7': attr_7_prob,
+            '8': attr_8_prob,
+            '9': attr_9_prob,
         })
 
     return attributes_probabilities
 
 
+def get_max_probability(data):
+    current_max = data[0]
+    for i in range(len(data)):
+        if (data[i]['probability'] > current_max['probability']):
+            current_max = data[i]
+    return current_max
+
+
 def naive_bayees(data, classes_probabilities, attributes_probabilities):
     accuracy = 0
     for i in range(len(data)):
-        democrat_sum = classes_probabilities['\'democrat\'']
-        republican_sum = classes_probabilities['\'republican\'']
+        class_0_prop = {'class': '0', 'probability': classes_probabilities['0']}
+        class_1_prop = {'class': '1', 'probability': classes_probabilities['1']}
+        class_2_prop = {'class': '2', 'probability': classes_probabilities['2']}
+        class_3_prop = {'class': '3', 'probability': classes_probabilities['3']}
+        class_4_prop = {'class': '4', 'probability': classes_probabilities['4']}
+        class_5_prop = {'class': '5', 'probability': classes_probabilities['5']}
+        class_6_prop = {'class': '6', 'probability': classes_probabilities['6']}
+        class_7_prop = {'class': '7', 'probability': classes_probabilities['7']}
+        class_8_prop = {'class': '8', 'probability': classes_probabilities['8']}
+        class_9_prop = {'class': '9', 'probability': classes_probabilities['9']}
 
         for j in range(len(data[i])-1):
-            if data[i][j] != '?':
-                democrat_sum *= attributes_probabilities[j]['\'democrat\''][data[i][j]]
-                republican_sum *= attributes_probabilities[j]['\'republican\''][data[i][j]]
+            class_0_prop['probability'] *= attributes_probabilities[j]['0']
+            class_1_prop['probability'] *= attributes_probabilities[j]['1']
+            class_2_prop['probability'] *= attributes_probabilities[j]['2']
+            class_3_prop['probability'] *= attributes_probabilities[j]['3']
+            class_4_prop['probability'] *= attributes_probabilities[j]['4']
+            class_5_prop['probability'] *= attributes_probabilities[j]['5']
+            class_6_prop['probability'] *= attributes_probabilities[j]['6']
+            class_7_prop['probability'] *= attributes_probabilities[j]['7']
+            class_8_prop['probability'] *= attributes_probabilities[j]['8']
+            class_9_prop['probability'] *= attributes_probabilities[j]['9']
 
-        #print(democrat_sum)
-        #print(republican_sum)
+        print(class_0_prop)
+        print(class_1_prop)
+        print(class_2_prop)
+        print(class_3_prop)
+        print(class_4_prop)
+        print(class_5_prop)
+        print(class_6_prop)
+        print(class_7_prop)
+        print(class_8_prop)
+        print(class_9_prop)
+        exit(0)
 
-        result = '\'democrat\'' if democrat_sum > republican_sum else '\'republican\''
-        expected = data[i][len(data[i])-1]
+        expected = data[i][0]
+        clasees_prop_list = [class_0_prop, class_1_prop, class_2_prop, class_3_prop, class_4_prop, class_5_prop, class_6_prop, class_7_prop, class_8_prop, class_9_prop]
+        result = get_max_probability(clasees_prop_list)
 
-        if result == expected:
+        print(result)
+
+        if result['class'] == expected:
             accuracy += 1
-            #print('   (E): ' + expected + '; (R): ' + result)
-        #else:
-            #print('Х: (E): ' + expected + '; (R): ' + result)
+            print('   (E): ' + expected + '; (R): ' + result['class'])
+        else:
+            print('Х: (E): ' + expected + '; (R): ' + result['class'])
 
     return (accuracy/len(data))*100
+
+
+def run_naive_bayees_with_chunks(data_chunks):
+    overall_accuracy = 0
+    for i in range(len(data_chunks)):
+        trainess_data = data_chunks[i]
+        test_data = data_chunks[:i] + data_chunks[i+1:len(data_chunks)]
+
+        classes_probabilities = init_classes_probabilities(trainess_data)
+        print(classes_probabilities)
+
+        attributes_probabilities = init_attributes_probabilities(trainess_data)
+        print_attributes_probabilitirs(attributes_probabilities)
+
+        accuracy = 0
+        for j in range(len(test_data)):
+            accuracy += naive_bayees(test_data[j], classes_probabilities, attributes_probabilities)
+
+        accuracy = accuracy / len(test_data)
+        overall_accuracy += accuracy
+        print('Chunk ' + str(i) + '; Accuracy: ' + str(accuracy) + '%')
+
+    overall_accuracy = overall_accuracy / len(data_chunks)
+    print('Overall accuracy: ' + str(overall_accuracy) + '%')
+
+
+if __name__ == "__main__":
+    print('Naive Bayees')
+
+    base_data = read_input_csv('datasets/train.test.csv')
+    data_chunks = separate_to_chunks(base_data, 10)
+
+    run_naive_bayees_with_chunks(data_chunks)
+
